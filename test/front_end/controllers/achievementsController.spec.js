ed7f5b56095bd0872778d33dbe46995767c9ad22
giamir <giamir.buoncristiani@gmail.com>
2016-02-02 16:10:02 +0000

    post reviews remove some superflous code

--- test/front_end/controllers/achievementsController.spec.js
+++ test/front_end/controllers/achievementsController.spec.js
@@ -1,5 +1,5 @@
 describe('AchievementsController', function() {
-  var data = {
+  var response = {
     data: [{ title: 'codewars', criteria: '150pts on codewars' }]
   };
   var ctrl;
@@ -14,15 +14,14 @@ describe('AchievementsController', function() {
   });
 
   beforeEach(inject(function($controller, $q, $rootScope) {
-    GetAchievementsFactoryMock.getData.and.returnValue($q.when(data));
+    GetAchievementsFactoryMock.getData.and.returnValue($q.when(response));
     ctrl = $controller('AchievementsController');
     scope = $rootScope;
   }));
 
   it('initializes with achievements from the GetAchievements Factory', function() {
-    ctrl.init();
     scope.$digest();
     expect(ctrl.achievements)
-      .toEqual([{ title: 'codewars', criteria: '150pts on codewars' }]);
+      .toEqual(response.data);
   });
 });
