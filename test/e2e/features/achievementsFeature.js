bde74acbd4bdc13090c6cef2ab226f5f3ec7748d
michaellennox <michaelctlennox@gmail.com>
2016-02-02 14:43:50 +0000

    Protrctor test for creating a new achievement written, awaiting name of repeater for achievementsList

---
+++ test/e2e/features/achievementsFeature.js
@@ -0,0 +1,27 @@
+describe('Achievements Features', function() {
+  it('a user is able to create a new achievement', function() {
+    browser.get('http://localhost:8080/#/achievements');
+
+    var achievementsList = element.all(by.repeater('achievement in ctrl.achievements'));
+
+    expect(achievementsList.length).toEqual(0);
+
+    var createAchievementLink = element(by.css('a[href*="#/achievements/new"]'));
+
+    createAchievementLink.click();
+
+    expect(browser.getCurrentUrl()).toContain('#/achievements/new');
+
+    var achievementTitleInput = element(by.css('input[name="title"]'));
+    var achievementCriteriaInput = element(by.css('input[name="criteria"]'));
+    var newAchievementForm = element(by.css('form'));
+
+    achievementTitleInput.sendKeys('Create an achievement for the app');
+    achievementCriteriaInput.sendKeys('This is where a user should write criteria');
+    newAchievementForm.submit();
+
+    expect(browser.getCurrentUrl()).toContain('#/achievements');
+    expect(achievementsList.length).toEqual(1);
+    expect(achievementsList.get(0).getText()).toEqual('Create an achievement for the app');
+  })
+})
