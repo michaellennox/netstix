6ddf48ee5d061fe597fb6e06a3cf2afac17b1784
giamir <giamir.buoncristiani@gmail.com>
2016-02-02 19:00:21 +0000

    drop test database before every protractor feature run

--- test/e2e/features/achievementsFeature.js
+++ test/e2e/features/achievementsFeature.js
@@ -1,10 +1,18 @@
+var mongoose = require('mongoose');
+
+beforeEach(function() {
+  mongoose.connect('mongodb://localhost/makers-achievements-test', function() {
+    mongoose.connection.db.dropDatabase();
+  });
+});
+
 describe('Achievements Features', function() {
   it('a user is able to create a new achievement', function() {
     browser.get('http://localhost:8080/#/achievements');
 
     var achievementsList = element.all(by.repeater('achievement in ctrl.achievements'));
 
-    expect(achievementsList).toEqual([]);
+    expect(achievementsList.count()).toEqual(0);
 
     var createAchievementLink = element(by.css('a[href*="#/achievements/new"]'));
 
@@ -20,6 +28,7 @@ describe('Achievements Features', function() {
     achievementCriteriaInput.sendKeys('This is where a user should write criteria');
     newAchievementForm.submit();
 
+    expect(achievementsList.count()).toEqual(1);
     expect(browser.getCurrentUrl()).toContain('#/achievements');
     expect(achievementsList.get(0).getText()).toEqual('Create an achievement for the app');
   });
