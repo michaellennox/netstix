b1fa7bb4c0821ae4f280d961e55f7e1697fe7eac
michaellennox <michaelctlennox@gmail.com>
2016-02-03 11:02:25 +0000

    User authentication routes are up and running

--- app/routes/users.js
+++ app/routes/users.js
@@ -4,17 +4,17 @@ var router = express.Router();
 var passport = require('passport');
 var User = require('../models/user');
 
-router.post('/', function() {
+router.post('/', function(req, res) {
   User.register(
     new User({
       username: req.body.username,
       password: req.body.password
-    }), function(err, account) {
+    }), req.body.password, function(err, account) {
       if(err) {
         return res.status(500).json({ err: err });
       }
       passport.authenticate('local')(req, res, function() {
-        return res.status(200).json({ status: 'Registration successful!' })
+        return res.status(200).json({ status: 'Registration successful!' });
       })
     }
   )
