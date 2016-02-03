abada199882f9139e29456ce6b2fa793cfcf2d74
giamir <giamir.buoncristiani@gmail.com>
2016-02-03 09:28:02 +0000

    wip protractor stop after first command

--- app.js
+++ app.js
@@ -13,11 +13,21 @@ var app = express();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(
+  function(req, res, next){
+    if (req.url === '/favicon.ico') {
+        res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+        res.end(/* icon content here */);
+    } else {
+        next();
+    }
+}
+);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(require('express-session')({
    secret: 'keyboard cat',
    resave: false,
