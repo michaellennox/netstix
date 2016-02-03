b2a4ece0bc1d7b6e8aafbfbebae69a1cf7309dbc
Thomas Bradley <bradley.tom.r@gmail.com>
2016-02-03 15:23:55 +0000

    Tidies up submission feature test and variable names

--- test/e2e/features/submissionsFeature.js
+++ test/e2e/features/submissionsFeature.js
@@ -16,35 +16,44 @@ describe('Submissions Features', function() {
   it('a user can make a submission for a specific achievement', function() {
 
     browser.get('http://localhost:8080/#/achievements');
+
     var signUpLink = element(by.css('a[href*="#/register"]'));
+
     signUpLink.click();
+
     var usernameInput = element(by.css('input[name="username"]'));
     var passwordInput = element(by.css('input[name="password"]'));
     var signUpForm = element(by.css('form'));
+
     usernameInput.sendKeys('test user');
     passwordInput.sendKeys('epicpassword');
     signUpForm.submit();
 
     var createAchievementLink = element(by.css('a[href*="#/achievements/new"]'));
+
     createAchievementLink.click();
 
     var achievementTitleInput = element(by.css('input[name="title"]'));
     var achievementCriteriaInput = element(by.css('input[name="criteria"]'));
     var newAchievementForm = element(by.css('form'));
+
     achievementTitleInput.sendKeys('Create an achievement for the app');
     achievementCriteriaInput.sendKeys('This is where a user should write criteria');
     newAchievementForm.submit();
+
     var achievementsList = element.all(by.repeater('achievement in ctrl.achievements'));
     var viewAchievementLink = achievementsList.get(0).element(by.css('a[href*="#/achievements/"]'));
+
     viewAchievementLink.click();
 
     var createSubmissionLink = element(by.css('a[href*="#/achievements/:id/submissions/new"]'));
+
     createSubmissionLink.click();
 
     expect(browser.getCurrentUrl()).toContain('/submissions/new');
 
-    var submissionLink = element(by.css('input[name="link"]'));
-    var submissionComment = element(by.css('input[name="comment"]'));
+    var submissionLinkInput = element(by.css('input[name="link"]'));
+    var submissionCommentInput = element(by.css('input[name="comment"]'));
     var newSubmissionForm = element(by.css('form'));
 
     submissionLinkInput.sendKeys('A link to the relevant code or example');
